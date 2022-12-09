a64cd943ef16ed8c16fd7fec7b78482fe912b7ff
tarrique-santos <109968671+tarrique-santos@users.noreply.github.com>
2022-12-09 16:08:39 -0300

    Add files via upload

---
+++ JS/chiss.js
@@ -0,0 +1,34 @@
+const txt_entrada = document.querySelector(".txt_entrada");
+
+const traduz = document.querySelector(".traduz");
+
+const resposta = document.querySelector(".resposta");
+
+const limpar = document.querySelector(".limpar");
+
+traduz.addEventListener("click", traducao);
+limpar.addEventListener("click", clean);
+
+function clean(){
+
+  document.querySelector(".txt_entrada").value="";
+}
+
+let url = "https://api.funtranslations.com/translate/enderman.json"
+
+// function traduzcao(text) {
+//   return url + "?" + "text=" + text
+// }
+
+async function traducao() {
+
+  const result = await fetch(`https://api.funtranslations.com/translate/enderman.json?text=${txt_entrada.value}`)
+  
+  .then(response=> response.json())
+  .then(response=>response.contents.translated)
+  // .catch(alert=("Error!"));
+
+  console.log(result);
+
+  resposta.innerHTML = result; 
+}
---
+++ JS/darth.js
@@ -0,0 +1,34 @@
+const txt_entrada = document.querySelector(".txt_entrada");
+
+const traduz = document.querySelector(".traduz");
+
+const limpar = document.querySelector(".limpar");
+
+const resposta = document.querySelector(".resposta");
+
+traduz.addEventListener("click", traducao);
+limpar.addEventListener("click", clean);
+
+function clean(){
+
+  document.querySelector(".txt_entrada").value="";
+}
+
+let url = "https://api.funtranslations.com/translate/sith.json";
+
+// function traduzcao(text) {
+//   return url + "?" + "text=" + text
+// }
+
+async function traducao() {
+
+  const result = await fetch(`https://api.funtranslations.com/translate/sith.json?text=${txt_entrada.value}`)
+  
+  .then(response=> response.json())
+  .then(response=>response.contents.translated)
+  .catch(alert=("Error!"));
+
+  console.log(result);
+
+  resposta.innerHTML = result; 
+}
